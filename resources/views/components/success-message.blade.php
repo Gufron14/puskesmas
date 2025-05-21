2cbba6edb9372d5951e23cace8ba9b18adb2209b
Dikha <nugrahaadi247@gmail.com>
2025-05-22 00:19:15 +0700

    add alert

---
+++ resources/views/components/success-message.blade.php
@@ -0,0 +1,8 @@
+<div class="alert alert-success alert-dismissible fade show" role="alert">
+    <div class="font-weight-bold">
+        {{ $action }}
+    </div>
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">×</span>
+    </button>
+</div>
