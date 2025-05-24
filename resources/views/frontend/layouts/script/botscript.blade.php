1dc302844789213007accb438f83ca67c0ec5495
Dikha <nugrahaadi247@gmail.com>
2025-05-24 18:13:30 +0700

    update script

--- resources/views/frontend/layouts/script/botscript.blade.php
+++ resources/views/frontend/layouts/script/botscript.blade.php
@@ -6,4 +6,31 @@
     <!-- * *                               SB Forms JS                               * *-->
     <!-- * * Activate your form at https://startbootstrap.com/solution/contact-forms * *-->
     <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+    <script src="{{ asset('backend/assets/js/jquery.min.js') }}"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
     <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+    <script>
+        function Logout() {
+            event.preventDefault();
+            const Logout = document.getElementById('logout-form');
+
+            Swal.fire({
+                title: 'Apakah Anda Yakin?',
+                text: "Anda akan keluar dari akun ini!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#2E93fA',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Keluar',
+                cancelButtonText: 'Batal',
+                reverseButtons: true,
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    $('#logout-form').submit();
+                }
+            });
+
+        }
+    </script>
--- resources/views/frontend/layouts/script/topscript.blade.php
+++ resources/views/frontend/layouts/script/topscript.blade.php
@@ -4,7 +4,7 @@
     <meta name="author" content="" />
     <title>SIM RM</title>
     <!-- Favicon-->
-    <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+<link rel="icon" href="{{ asset('backend/assets/logo/logo.png') }}">
     <!-- Font Awesome icons (free version)-->
     <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
     <!-- Google fonts-->
