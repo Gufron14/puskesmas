2723166cc1ea908469327b80035c620d46df296e
Gufron <gupron.nurjalil14@gmail.com>
2025-07-31 01:48:50 +0700

    fe user

--- resources/views/frontend/rekammedis.blade.php
+++ resources/views/frontend/rekammedis.blade.php
@@ -14,7 +14,7 @@
                     </div>
                 @else
                     <div class="table-responsive">
-                        <table class="table table-bordered table-striped align-middle">
+                        <table class="table table-bordered table-sm align-middle">
                             <thead class="table-primary text-center">
                                 <tr>
                                     <th>Tanggal Pemeriksaan</th>
@@ -25,7 +25,7 @@
                                     <th>Diagnosa</th>
                                     <th>Catatan Dokter</th>
                                     <th>Resep Obat</th>
-                                    <th>PDF</th>
+                                    <th>Aksi</th>
                                 </tr>
                             </thead>
                             <tbody>
@@ -50,17 +50,10 @@
                                         <td class="text-end">Rp{{ number_format($totalObat, 0, ',', '.') }}</td>
                                         <td class="text-end fw-bold text-primary">Rp{{ number_format($totalObat + ($item->biaya ?? 0), 0, ',', '.') }}</td> --}}
                                         <td>
-                                            @if (count($resep))
-                                                <ul class="list-unstyled mb-0">
-                                                    @foreach ($resep as $r)
-                                                        <li>
-                                                            <strong>{{ $r['nama_obat'] ?? '-' }}</strong>
-                                                        </li>
-                                                    @endforeach
-                                                </ul>
-                                            @else
-                                                <em>-</em>
-                                            @endif
+                                            <button type="button" class="btn btn-primary btn-sm" data-bs-toggle="modal"
+                                                data-bs-target="#exampleModal">
+                                                Lihat Resep
+                                            </button>
                                         </td>
                                         <td class="text-center">
                                             <a href="{{ route('pemeriksaan.exportPdf', $item->id) }}" target="_blank"
@@ -79,6 +72,71 @@
                     Silakan <a href="{{ route('login') }}">login</a> terlebih dahulu untuk melihat riwayat rekam medis Anda.
                 </div>
             @endauth
+        </div>
+
+        <!-- Modal -->
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-lg">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="exampleModalLabel">Resep Obat</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+@if (count($resep))
+    <table class="table table-bordered">
+        <thead>
+            <tr>
+                <th>No</th>
+                <th>Nama Obat</th>
+                <th>Aturan Minum</th>
+                <th>Jumlah</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($resep as $index => $r)
+                <tr>
+                    <td>{{ $index + 1 }}</td>
+                    <td>{{ $r['nama_obat'] ?? '-' }}</td>
+                    <td>{{ $r['keterangan_makan'] ?? '-' }}</td>
+                    <td>
+                        {{ $r['jumlah'] ?? 0 }}
+                        @switch($r['jenis_obat'])
+                            @case('Serbuk')
+                                Kantong
+                                @break
+                            @case('Kapsul')
+                                Kapsul
+                                @break
+                            @case('Tablet')
+                                Tablet
+                                @break
+                            @case('Sirup')
+                            @case('Obat Tetes')
+                                Botol
+                                @break
+                            @case('Salep')
+                                Pcs
+                                @break
+                            @default
+                                -
+                        @endswitch
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+@else
+    <em>-</em>
+@endif
+
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tutup</button>
+                        {{-- <button type="button" class="btn btn-primary">Tutup</button> --}}
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
 @endsection
